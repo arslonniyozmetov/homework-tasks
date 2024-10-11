934b4300dfd8ed5c03596a904883ccad0a416d92
arslonniyozmetov <arslonniyozmetov@gmail.com>
2024-10-11 16:24:08 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+def tugilgan_yil_hisobla(j_yil=2024):
+    ism=input("Ismingizni kiriting: ")
+    yosh=int(input("Yoshingizni kiriting:"))
+    print(f"{ism}, siz {j_yil-yosh} yilda tugilgansiz")
+tugilgan_yil_hisobla()
